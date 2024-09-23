4c641c41d4be37024a22e652c42e7ce4f462cfd8
sudhanshu201 <113603064+sudhanshu201@users.noreply.github.com>
2024-09-23 16:33:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,46 @@
 //your JS code here. If required.
+document.getElementById("btn").addEventListener("click", function (e){
+        //prevent form default behaviour
+        e.preventDefault();
+
+        //get the input values
+        let age = document.getElementById("age").value 
+        let name = document.getElementById("name").value 
+
+        //ensure fields are not empty
+
+        if(age === "" || name === ""){
+            alert("please enter both your name and age")
+            return
+        }
+        
+        // Convert age to a number for comparison
+            age = Number(age);
+
+
+        //create a promise
+
+       let myPromise =  new Promise((resolve,reject)=>{
+            setTimeout(() => {
+                if(age>=18){
+                    resolve("eligible")
+                } else {
+                    reject("not eligible")
+                }
+            }, 4000);
+        })
+
+        myPromise.then((res)=>{
+            if(res==="eligible") {
+                alert(`Welcome ${name}. You can vote`)
+            
+            }
+
+        }).catch((e)=>{
+            if(e==="not eligible"){
+                alert(`Oh sorry, ${name}. You aren't old enough `)
+              
+            }
+        })
+}
+)
